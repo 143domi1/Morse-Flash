4b091737990778cb01815522e15c75231d3655ef
Dominik <93765669+143domi1@users.noreply.github.com>
2026-07-24 14:12:33 +0200

    changed version numbers

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.g143domi1.morseflash"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
